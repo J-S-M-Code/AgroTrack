cbdbe777304266ebf6d9e47f7ce8a4a4eb222d2d
Joaco Suilar Murcia <69277685+J-S-M-Code@users.noreply.github.com>
2025-04-26 23:39:15 -0300

    First Commit

---
+++ createCollections.bat
@@ -0,0 +1,13 @@
+@echo off
+echo use agroTrack > mongo-setup.js
+echo db.createCollection("dueños") >> mongo-setup.js
+echo db.createCollection("animales") >> mongo-setup.js
+echo db.createCollection("alertas") >> mongo-setup.js
+echo db.createCollection("campos") >> mongo-setup.js
+echo db.createCollection("usuarios") >> mongo-setup.js
+
+docker exec -i agroTrack mongosh < mongo-setup.js
+
+del mongo-setup.js
+
+pause
